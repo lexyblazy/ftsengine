64b08c61ad5e9a6ca7f114608773cbb88f9f8ecf
lexyblazy <adekola.olalekan01@gmail.com>
2024-09-22 02:48:05 +0200

    implement ranking for partial matches

--- engine/engine.go
+++ engine/engine.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"time"
 
@@ -23,6 +24,7 @@ type SearchResults struct {
 	Meta struct {
 		Count         int    `json:"count"`
 		TimeTakenSecs string `json:"timeTaken"`
+		Query         string `json:"searchQuery"`
 	} `json:"meta"`
 	Data []index.Document `json:"data"`
 }
@@ -54,20 +56,21 @@ func (f *FtsEngine) buildIndex() {
 	f.index.SaveMeta(metaFieldName, recordsCount)
 
 	ch <- true
-	fmt.Printf("\n Index built in %s ✅✅. No of tokens = %v  \n", time.Since(start), recordsCount)
+	log.Printf("\n Index built in %s ✅✅. No of tokens = %v  \n", time.Since(start), recordsCount)
 
 }
 
-func (f *FtsEngine) Search(text string) []byte {
+func (f *FtsEngine) getDocs(text string, exactResults bool) []index.Document {
 
-	start := time.Now()
 	var r []int
 
-	for _, token := range analyzer.Analyze(text) {
+	searchTokens := analyzer.Analyze(text)
+
+	for _, token := range searchTokens {
 		val := f.index.GetFromInvertedIndex(token)
 
 		if val == "" {
-			break // use continue for OR queries
+			continue
 
 		}
 
@@ -76,12 +79,14 @@ func (f *FtsEngine) Search(text string) []byte {
 		if r == nil {
 			r = ids
 		} else {
-			r = utils.Intersection(r, ids)
+			if exactResults {
+				r = utils.Intersection(r, ids) // exact matches
+			} else {
+				r = append(r, ids...) // partial to exact matches
+			}
 		}
 
 	}
-
-	results := SearchResults{}
 
 	docs := []index.Document{}
 
@@ -97,9 +102,60 @@ func (f *FtsEngine) Search(text string) []byte {
 
 	}
 
+	// no need to rank if results are exact matches
+	if exactResults {
+		return docs
+	}
+
+	return f.rankResults(docs, searchTokens)
+
+}
+
+func (f *FtsEngine) rankResults(docs []index.Document, searchTokens []string) []index.Document {
+
+	// for each document, calculate the relevance by counting the amount of
+	// searchTokens that can be found in the document.Text
+	for i := range docs {
+		score := 0
+
+		resultTokens := analyzer.Analyze(docs[i].Text)
+
+		resultTokensMap := make(map[string]bool)
+
+		for _, resultToken := range resultTokens {
+			resultTokensMap[resultToken] = true
+		}
+
+		for _, searchToken := range searchTokens {
+			if _, exists := resultTokensMap[searchToken]; exists {
+				score++
+			}
+
+		}
+
+		docs[i].Rank = (float64(score) / float64(len(searchTokens)))
+	}
+
+	// order by relevance...
+	sort.Slice(docs, func(i, j int) bool {
+		return docs[i].Rank > docs[j].Rank
+	})
+
+	return docs
+
+}
+
+func (f *FtsEngine) Search(text string, exactResults bool) []byte {
+
+	start := time.Now()
+	docs := f.getDocs(text, exactResults)
+
+	results := SearchResults{}
+
 	results.Data = docs
 	results.Meta.Count = len(docs)
 	results.Meta.TimeTakenSecs = fmt.Sprintf("%.9f seconds", time.Since(start).Seconds())
+	results.Meta.Query = text
 
 	res, err := json.Marshal(results)
 
@@ -182,7 +238,7 @@ func (f *FtsEngine) loadDocuments(path string) error {
 	recordsCount := f.index.BulkSave(wb)
 	f.index.SaveMeta(metaFieldName, recordsCount)
 
-	fmt.Printf("\n %v Documents loaded in %s ✅✅ \n", recordsCount, time.Since(start))
+	log.Printf("\n %v Documents loaded in %s ✅✅ \n", recordsCount, time.Since(start))
 
 	ch <- true
 
